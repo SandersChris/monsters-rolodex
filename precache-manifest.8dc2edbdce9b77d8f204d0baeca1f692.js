22798cfb2e4985a28d462eadea0246dc9e319fe4
Chris Sanders <christopherdksanders@gmail.com>
2020-02-02 11:30:36 -0800

    Updates

---
+++ precache-manifest.8dc2edbdce9b77d8f204d0baeca1f692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817ef5b5b8438a8feabb41a517e2f676",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c43b230cf9516886b12b",
+    "url": "/monsters-rolodex/static/css/main.0c266b79.chunk.css"
+  },
+  {
+    "revision": "fcdd128eef2034e2c8ea",
+    "url": "/monsters-rolodex/static/js/2.4fc5497d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.4fc5497d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c43b230cf9516886b12b",
+    "url": "/monsters-rolodex/static/js/main.19cfcf15.chunk.js"
+  },
+  {
+    "revision": "1672be7e2e42305550d3",
+    "url": "/monsters-rolodex/static/js/runtime-main.8a2e9529.js"
+  }
+]);
